70600a8095e21a313f66b400166ca99862a3b365
Karl <64211272+kirillPro2020@users.noreply.github.com>
2025-01-13 22:32:24 +0700

    Add files via upload

---
+++ js/Fade_in.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const carousel = document.querySelector('.carousel');
+    carousel.classList.add('show'); // Добавляем класс для плавного появления
+});
+
