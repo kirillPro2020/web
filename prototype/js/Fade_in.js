c54c6dfcf9493cdb8841ef121e90f3e22d80674e
Karl <64211272+kirillPro2020@users.noreply.github.com>
2025-01-13 22:51:27 +0700

    Delete prototype directory

--- prototype/js/Fade_in.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    const carousel = document.querySelector('.carousel');
-    carousel.classList.add('show'); // Добавляем класс для плавного появления
-});
-
